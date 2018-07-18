679da13d2be3a80a3374f84b0c37fcb30f59815f
mattiehansen <mattiehansen@unomaha.edu>
2018-07-18 17:57:09 -0500

    Initial test scripts

--- schoolpass/tests.py
+++ schoolpass/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Student,Teacher,Pass
+from .forms import LoginForm, PassForm, ReturnForm
 
-# Create your tests here.
+class Test(TestCase):
+
+    def create_whatever(self,
+                        fn='firstname',
+                        ln='lastname',
+                        id=1000000001,
+                        ps='password',
+                        ac='accom here',
+                        n='notes here',
+                        pn=0,
+                        rn=0,):
+        return Student.objects.create(student_first_name=fn,
+                                      student_last_name=ln,
+                                      student_id=id,
+                                      student_password=ps,
+                                      accommodations=ac,
+                                      notes=n,
+                                      number_of_passes=pn,
+                                      number_of_rejections=rn)
+
+    def test_whatever_creation(self):
+        w = self.create_whatever()
+        self.assertTrue(isinstance(w, Student))
+        self.assertEqual(w.__str__(),
+                         w.student_first_name,
+                         w.student_last_name,)
