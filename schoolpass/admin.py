15e5defcf3f2d3f99271e0c4512316f65061b890
mattiehansen <mattiehansen@unomaha.edu>
2018-08-07 01:26:11 -0500

    Models 95% completed and displayed on admin page

--- schoolpass/admin.py
+++ schoolpass/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
-from .models import Student, Teacher, Pass
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Pass)
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser, Student, Teacher, Pass
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'first_name', 'last_name', 'last_login', 'classes',
+                    'is_staff', 'is_superuser', 'is_active', 'is_student',]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    model = Student
+    list_display = ['__str__', 'user', 'classes', 'last_logged_in',  'accommodations',
+                    'notes', 'number_of_passes', 'number_of_rejections', ]
+
+class TeacherAdmin(admin.ModelAdmin):
+    model = Teacher
+    list_display = ['__str__', 'user', 'classes', 'last_logged_in',  'room_number', ]
+
+class PassAdmin(admin.ModelAdmin):
+    model = Pass
+    list_display = ['student', 'teacher', 'location',  'time_left', 'time_returned']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Pass, PassAdmin)
--- schoolpass/models.py
+++ schoolpass/models.py
@@ -1,100 +1,70 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from datetime import datetime
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
-#https://stackoverflow.com/questions/20779068/setting-up-two-different-types-of-users-in-django-1-5-1-6
-#https://wsvincent.com/django-custom-user-model-tutorial/
-#https://stackoverflow.com/questions/30495979/django-1-8-multiple-custom-user-types
-#https://wsvincent.com/django-custom-user-model-tutorial/
-#https://stackoverflow.com/questions/48011275/custom-user-model-fields-abstractuser-not-showing-in-django-admin
-#add email field
-'''
-class UserManager(BaseUserManager):
-    pass
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class MyUser(AbstractBaseUser):
-    personal_id = models.PositiveIntegerField(
-        validators=[MinValueValidator(1000000000), MaxValueValidator(9999999999)],
-        unique=True,)
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
-    email = models.EmailField(max_length=100)
 
-    active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
+class CustomUser(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    classes = models.TextField(blank=True)
 
-    USERNAME_FIELD = 'personal_id'
-    REQUIRED_FIELDS = []
-
-'''
+    def __str__(self):
+        return self.username
 
 
 class Student(models.Model):
-    id_student = models.AutoField(primary_key=True)
-    student_first_name = models.CharField(max_length=15)
-    student_last_name = models.CharField(max_length=15)
-    student_id = models.PositiveIntegerField(
-        validators=[MinValueValidator(1000000000), MaxValueValidator(9999999999)],
-        unique=True,)
-    student_password = models.CharField(max_length=25)
-    accommodations = models.TextField(default='None')
-    notes = models.TextField(default='No notes')
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True, related_name='student_profile')
+    accommodations = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
     number_of_passes = models.PositiveIntegerField(default=0)
     number_of_rejections = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return self.student_first_name + " " + self.student_last_name
+        return self.user.first_name + " " + self.user.last_name
 
-    def get_id(self):
-        return self.student_id
+    def last_logged_in(self):
+        return self.user.last_login
 
-    def get_accommodations(self):
-        return self.accommodations
-
-    def get_notes(self):
-        return self.notes
-
-    def get_passes(self):
-        return self.number_of_passes
-
-    def get_rejections(self):
-        return self.number_of_rejections
-
+    def classes(self):
+        return self.user.classes
 
 class Teacher(models.Model):
-    id_teacher = models.AutoField(primary_key=True)
-    teacher_first_name = models.CharField(max_length=15)
-    teacher_last_name = models.CharField(max_length=15)
-    teacher_id = models.PositiveIntegerField(
-        validators=[MinValueValidator(1000000000), MaxValueValidator(9999999999)],
-        unique=True,)
-    departments = models.CharField(max_length=50)
-    room_number = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)])
-    teacher_password = models.CharField(max_length=25)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True, related_name='teacher_profile')
+    room_number = models.PositiveSmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(9999)])
 
     def __str__(self):
-        return self.teacher_first_name + " " + self.teacher_last_name
+        return self.user.first_name + " " + self.user.last_name
 
-    def get_departments(self):
-        return self.departments
+    def last_logged_in(self):
+        return self.user.last_login
 
-    def get_room_number(self):
-        return self.room_number
+    def classes(self):
+        return self.user.classes
 
 class Pass(models.Model):
-    id_pass = models.AutoField(primary_key=True)
-    WATERFOUNTAIN = 'WF'
-    RESTROOM = 'RR'
-    NURSE = 'N'
-    OFFICE = 'O'
-    OTHERCLASSROOM = 'OC'
-    PASS_CHOICES = (
-        (WATERFOUNTAIN, 'Water Fountain'),
-        (RESTROOM, 'Sophomore'),
-        (NURSE, 'Nurse'),
-        (OFFICE, 'Office'),
-        (OTHERCLASSROOM, 'Other Classroom'),
-    )
-    kind_of_pass = models.CharField(max_length=15, choices=PASS_CHOICES, default=WATERFOUNTAIN)
-    date_time = models.DateTimeField(default=datetime.now, blank=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30)
+    time_left = models.DateTimeField(default=datetime.now, blank=True)
+    time_returned = models.DateTimeField(blank=True)
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    print('****', created)
+    if instance.is_student is True:
+        Student.objects.get_or_create(user=instance)
+    else:
+        Teacher.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    print('_-----')
+    if instance.is_student is True:
+        instance.student_profile.save()
+    else:
+        Teacher.objects.get_or_create(user=instance)
+
+
